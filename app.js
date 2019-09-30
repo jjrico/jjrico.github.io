ba231fe404b8abf13aa8f598a886c9ec64d082a9
Jeremy Rico <jjrico@csu.fullerton.edu>
2019-09-30 16:25:59 -0700

    firebaseUI

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 //function() {
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyC6HOV_x318odYmVQaiaUgljdI9r5hO838",
-    authDomain: "raspberrydrive.me",
-    databaseURL: "https://raspberry-drive-f3071.firebaseio.com",
-    projectId: "raspberry-drive-f3071",
-    storageBucket: "",
-    messagingSenderId: "1097525356797",
-    appId: "1:1097525356797:web:7b68a121605052777df694",
-    measurementId: "G-02T7NMCWE8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+  <script src="https://www.gstatic.com/firebasejs/7.0.0/firebase-app.js"></script>
+
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+       https://firebase.google.com/docs/web/setup#available-libraries -->
+  <script src="https://www.gstatic.com/firebasejs/7.0.0/firebase-analytics.js"></script>
+
+  <script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyC6HOV_x318odYmVQaiaUgljdI9r5hO838",
+      authDomain: "raspberry-drive-f3071.firebaseapp.com",
+      databaseURL: "https://raspberry-drive-f3071.firebaseio.com",
+      projectId: "raspberry-drive-f3071",
+      storageBucket: "",
+      messagingSenderId: "1097525356797",
+      appId: "1:1097525356797:web:7b68a121605052777df694",
+      measurementId: "G-02T7NMCWE8"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  </script>
 
 //}
