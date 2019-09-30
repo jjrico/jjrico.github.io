9f13d15b875d396f0753c0418947199ac6ddfd3f
Jeremy Rico <jjrico@csu.fullerton.edu>
2019-09-30 16:08:48 -0700

    firebaseUI

---
+++ app.js
@@ -0,0 +1,18 @@
+//function() {
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC6HOV_x318odYmVQaiaUgljdI9r5hO838",
+    authDomain: "raspberrydrive.me",
+    databaseURL: "https://raspberry-drive-f3071.firebaseio.com",
+    projectId: "raspberry-drive-f3071",
+    storageBucket: "",
+    messagingSenderId: "1097525356797",
+    appId: "1:1097525356797:web:7b68a121605052777df694",
+    measurementId: "G-02T7NMCWE8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+//}
