d3650bb301139c6691988d03b1c412ca0f2e836a
mcabrera1997 <43303898+mcabrera1997@users.noreply.github.com>
2019-12-03 22:49:11 -0800

    Add files via upload

---
+++ Raspberry_Drive/rbdrive/__init__.py
@@ -0,0 +1,16 @@
+#sources: https://www.youtube.com/channel/UCCezIgC97PvUuR4_gbFUs5g
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from rbdrive import routes
